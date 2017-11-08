fb523322941280bd5ca13f0ae0c02aef9f4d13f8
Fam'Hongtra <anucha.it20@mail.kmutt.com>
2017-11-08 19:46:07 +0700

    Edited Function Send Username & Password by Email

--- resources/views/emails/mailme.blade.php
+++ resources/views/emails/mailme.blade.php
@@ -1,11 +1,13 @@
 
 <h2>Admintools Password Generator</h2>
--------------------------------------------------------------
+<hr>
 <p>Use this username & password to login.</p><br>
 
 <h3>username: {{$username}}</h3>
 <h3>password: {{$password}}</h3><br>
 
-<h3>Account was created by {{$generated_by}}</h3>
--------------------------------------------------------------
+<h4>Account was created by {{$generated_by}}</h4>
+<hr>
+<p>Go to the following web address:</p>
+<a href="https://sit-admintools.me">https://sit-admintools.me</a>
 <p>Please do not reply to this mail.</p>
--- resources/views/loginpage.blade.php
+++ resources/views/loginpage.blade.php
@@ -5,7 +5,8 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <title>Laravel</title>
+  <title>Admintools</title>
+  <link rel="shortcut icon" href="img/logoicon.png" />
 
   <!-- Fonts -->
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
